423e5fe10fda6376bc9470dc39f7bf457e025af2
Ilya Solovyov <mail@erdenian.ru>
2020-04-13 21:07:33 +0300

    Initial commit

---
+++ src/mingwMain/kotlin/Main.kt
@@ -0,0 +1,44 @@
+import kotlinx.cinterop.convert
+import kotlinx.cinterop.pointed
+import kotlinx.cinterop.toKString
+import platform.posix.closedir
+import platform.posix.opendir
+import platform.posix.readdir
+import platform.windows.MB_ICONQUESTION
+import platform.windows.MB_OK
+import platform.windows.MessageBoxW
+
+fun main() {
+    val supportedExtensions = listOf(".gsba", ".rar", ".zip")
+
+    val dublicates = checkNotNull(listFiles("./")).asSequence()
+        .filter { name -> supportedExtensions.any { name.endsWith(it) } }
+        .map { name ->
+            if (name.endsWith(".gsba")) name.takeWhile { it != '_' }
+            else name.dropLastWhile { it != '.' }.dropLast(1)
+        }.groupingBy { it }
+        .eachCount()
+        .filter { it.value > 1 }
+        .map { (name, count) ->
+            "$name ($count times)"
+        }
+    MessageBoxW(
+        null, dublicates.takeIf { it.isNotEmpty() }?.joinToString("\n") ?: "No dublicates",
+        "Dublicates", (MB_OK or MB_ICONQUESTION).convert()
+    )
+}
+
+@OptIn(ExperimentalStdlibApi::class)
+private fun listFiles(path: String): List<String>? = opendir(path)?.let { directory ->
+    buildList {
+        try {
+            var info = readdir(directory)
+            while (info != null) {
+                add(info.pointed.d_name.toKString())
+                info = readdir(directory)
+            }
+        } finally {
+            closedir(directory)
+        }
+    }
+}
